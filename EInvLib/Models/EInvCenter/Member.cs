a338517524b46b577ea24bc132775da4216e84cc
Illus Wu <illuserus@gmail.com>
2019-05-31 05:24:09 +0800

    [add]add some Model

---
+++ EInvLib/Models/EInvCenter/Member.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EInvLib.Models.EInvCenter
+{
+    class Member
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public string Account { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string EMail { get; set; }
+        public int IsActived { get; set; }
+
+        [InverseProperty("SellerInfo")]
+        public ICollection<SellerInfo> Sellers;
+
+    }
+}
